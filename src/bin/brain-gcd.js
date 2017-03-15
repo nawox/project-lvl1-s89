50ceb596ff3adc3e55454fa20f3735d7a02336f5
Vladimir Kopanev <kopanev42@gmail.com>
2017-03-15 21:30:59 +0000

    sixth step

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env nodejs
+
+import startGame from '../games/game';
+import gameCond from '../games/gcd';
+
+startGame(gameCond());
---
+++ src/games/gcd.js
@@ -0,0 +1,30 @@
+const gameRule = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (x, y) => {
+  if (x <= 0 || y <= 0) {
+    return x + y;
+  }
+  if (x > y) {
+    return getGcd(x % y, y);
+  }
+  return getGcd(x, y % x);
+};
+
+const getQuest = () => {
+  const number1 = Math.floor(Math.random() * 100);
+  const number2 = Math.floor(Math.random() * 100);
+  const struct = {
+    question: `${number1} ${number2}`,
+    answer: String(getGcd(number1, number2)),
+  };
+
+  return struct;
+};
+
+export default () => {
+  const gameCond = {
+    getQuestAnswer: getQuest,
+    rule: gameRule,
+  };
+  return gameCond;
+};
